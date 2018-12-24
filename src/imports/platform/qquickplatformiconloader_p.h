28699c95c29281147449cdac7ebd7e2827e9b989
J-P Nurmi <jpnurmi@gmail.com>
2018-12-24 20:00:06 +0100

    Remove inapplicable private API warnings

--- src/imports/platform/qquickplatformiconloader_p.h
+++ src/imports/platform/qquickplatformiconloader_p.h
@@ -23,17 +23,6 @@
 #ifndef QQUICKPLATFORMICONLOADER_P_H
 #define QQUICKPLATFORMICONLOADER_P_H
 
-//
-//  W A R N I N G
-//  -------------
-//
-// This file is not part of the Qt API.  It exists purely as an
-// implementation detail.  This header file may change from version to
-// version without notice, or even be removed.
-//
-// We mean it.
-//
-
 #include <QtCore/qurl.h>
 #include <QtCore/qstring.h>
 #include <QtGui/qicon.h>

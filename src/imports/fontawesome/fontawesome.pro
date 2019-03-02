b909a3dd2d2fdabeedfc3b8b91aee651c71c1aae
J-P Nurmi <jpnurmi@gmail.com>
2019-03-02 19:48:52 +0100

    Add FontAwesome

---
+++ src/imports/fontawesome/fontawesome.pro
@@ -0,0 +1,26 @@
+TARGET = qtcellinkfontawesomeplugin
+TARGETPATH = QtCellink/FontAwesome
+IMPORT_VERSION = 1.0
+
+QT += qml
+
+DEFINES += QT_NO_CAST_TO_ASCII QT_NO_CAST_FROM_ASCII
+
+QML_FILES += \
+    $$PWD/FontAwesome.qml
+
+OTHER_FILES += \
+    $$PWD/qmldir \
+    $$QML_FILES
+
+SOURCES += \
+    $$PWD/qtcellinkfontawesomeplugin.cpp
+
+RESOURCES += \
+    fontawesome.qrc
+
+CONFIG += no_cxx_module builtin_resources qtquickcompiler
+load(qml_plugin)
+
+### TODO: fix qml_module.prf (no way to turn off install_qml_files)
+qmldir.files = $$qmldir_file
---
+++ src/imports/fontawesome/qtcellinkfontawesomeplugin.cpp
@@ -0,0 +1,39 @@
+/****************************************************************************
+**
+** Copyright (C) 2019 CELLINK AB <info@cellink.com>
+**
+** This file is part of QtCellink.
+**
+** QtCellink is free software: you can redistribute it and/or modify it
+** under the terms of the GNU Lesser General Public License as published
+** by the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** QtCellink is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with QtCellink. If not, see <https://www.gnu.org/licenses/>.
+**
+****************************************************************************/
+
+#include <QtQml/qqmlextensionplugin.h>
+#include <QtQml/qqml.h>
+
+class QtCellinkFontAwesomePlugin: public QQmlExtensionPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID QQmlExtensionInterface_iid)
+
+public:
+    explicit QtCellinkFontAwesomePlugin(QObject *parent = nullptr) : QQmlExtensionPlugin(parent) { }
+
+    void registerTypes(const char *uri) override
+    {
+        qmlRegisterSingletonType(QStringLiteral("qrc:/qt-project.org/imports/QtCellink/FontAwesome/FontAwesome.qml"), uri, 1, 0, "FontAwesome");
+    }
+};
+
+#include "qtcellinkfontawesomeplugin.moc"

18ee4c135cbd135a0030671dac0c5d8452d2b9c3
J-P Nurmi <jpnurmi@gmail.com>
2020-11-26 13:03:04 +0100

    Fix static build on Windows
    Don't attempt to re-define Q_CELLINK_EXPORT if defined from the outside.

--- src/cellink/core/cellink.h
+++ src/cellink/core/cellink.h
@@ -25,14 +25,16 @@
 
 #include <QtCore/qglobal.h>
 
-#if !defined(QT_STATIC) && !defined(Q_CELLINK_EXPORT)
-#  if defined(QT_BUILD_CELLINK_LIB)
-#    define Q_CELLINK_EXPORT Q_DECL_EXPORT
+#ifndef Q_CELLINK_EXPORT
+#  ifndef QT_STATIC
+#    if defined(QT_BUILD_CELLINK_LIB)
+#      define Q_CELLINK_EXPORT Q_DECL_EXPORT
+#    else
+#      define Q_CELLINK_EXPORT Q_DECL_IMPORT
+#    endif
 #  else
-#    define Q_CELLINK_EXPORT Q_DECL_IMPORT
+#    define Q_CELLINK_EXPORT
 #  endif
-#else
-#  define Q_CELLINK_EXPORT
 #endif
 
 #endif // CELLINK_H
